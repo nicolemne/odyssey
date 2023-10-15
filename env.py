9fbddfad56a900ea3eaeac8c4ff26c66bb6a5c84
Nicole MN.E <mcnultyengstrom@gmail.com>
2023-10-15 16:35:41 +0000

    New credentials

--- env.py
+++ env.py
@@ -2,9 +2,9 @@ import os
 
 # os.environ["DEV"] = "1"
 os.environ["DEBUG"] = "1"
-os.environ["SECRET_KEY"] = "Odyssey@dsi!Travel_Blog"
+os.environ["SECRET_KEY"] = "TravelInsta123!Odyssey5PLastProject:)"
 os.environ["ALLOWED_HOST"] = "https://nicolemne-odyssey-zuyff703h8c.ws-eu105.gitpod.io/"
 os.environ["CLIENT_ORIGIN"] = "https://nicolemne-odyssey-zuyff703h8c.ws-eu105.gitpod.io/"
-os.environ["CLOUDINARY_URL"] = "cloudinary://849145316924133:y-_m_eGe8IChKTNSoFgDLg-X6qU@dl1c70wmm"
-os.environ["DATABASE_URL"] = "postgres://ptzimokk:9caCLxBfUVXDA-DVPqK49yFv5Ryuvxkv@cornelius.db.elephantsql.com/ptzimokk"
+os.environ["CLOUDINARY_URL"] = "cloudinary://919735918575891:rYnDU1DkLNw1UdYjaZ877fK_VAY@dohlebn4a"
+os.environ["DATABASE_URL"] = "postgres://korhkbjc:e9-td-jmv_3xeUS_X8FPT4KRqPMTNL-q@cornelius.db.elephantsql.com/korhkbjc"
 os.environ["DISABLE_COLLECTSTATIC"] = "1"
