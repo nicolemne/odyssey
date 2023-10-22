d97582d291974aeb8b57cb7464e3f40f38291a1b
Nicole MN.E <mcnultyengstrom@gmail.com>
2023-10-22 15:05:02 +0000

    Redo Navbar styling and layout

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import Button from 'react-bootstrap/Button';
 import './App.module.css';
-import styles from './styles/NavBar.module.css';
+import styles from './App.module.css';
 import NavBar from './components/NavBar';
 
 function App() {
--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -1,46 +1,32 @@
-import React from 'react';
-import styles from '../styles/NavBar.module.css';
+import React from "react";
 import { Navbar, Container, Nav } from "react-bootstrap";
-import logo from '../assets/logo.png';
+import logo from "../assets/logo.png";
+import styles from "../styles/NavBar.module.css";
 
 const NavBar = () => {
-    return (
-        <Navbar className={styles.NavBar} expand="md" fixed="top">
-            <Navbar.Brand>
-                <img 
-                    src={logo} 
-                    alt="logo" 
-                    height="190" 
-                    width="190"
-                />
-            </Navbar.Brand>
-            <Container>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="text-left flex-column">
-                        <Nav.Link>
-                            <span className={styles.NavItem}>
-                                <i className={`fas fa-home ${styles.NavIcon}`}></i>
-                                <p>Home</p>
-                            </span>
-                        </Nav.Link>
-                        <Nav.Link>
-                            <span className={styles.NavItem}>
-                                <i className={`fas fa-sign-in-alt ${styles.NavIcon}`}></i>
-                                <p>Sign in</p>
-                            </span>
-                        </Nav.Link>
-                        <Nav.Link>
-                            <span className={styles.NavItem}>
-                                <i className={`fas fa-user-plus ${styles.NavIcon}`}></i>
-                                <p>Sign up</p>
-                            </span>
-                        </Nav.Link>
-                    </Nav>
-                </Navbar.Collapse>
-            </Container>
-        </Navbar>
-    );
-}
+  return (
+    <Navbar className={styles.NavBar} expand="md" fixed="top">
+      <Container>
+        <Navbar.Brand>
+          <img src={logo} alt="logo" height="45" />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ml-auto text-left">
+            <Nav.Link>
+              <i className="fas fa-home"></i>Home
+            </Nav.Link>
+            <Nav.Link>
+              <i className="fas fa-sign-in-alt"></i>Sign in
+            </Nav.Link>
+            <Nav.Link>
+              <i className="fas fa-user-plus"></i>Sign up
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
 
 export default NavBar;
